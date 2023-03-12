8db49ec8e0ed427c5d786f468091e176545c7512
Gonçalo Alves <goncalo.alves@ua.pt>
2023-03-12 23:42:42 +0000

    updates

--- dashboard/php/crud.php
+++ dashboard/php/crud.php
@@ -1,16 +1,203 @@
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "porto1893";
-    $port = 3306;
-    $dbname = "dashboard";
+<!DOCTYPE html>
+<html lang="en">
 
-    $conn = mysqli_connect($servername ,$username, $password, $dbname, $port);
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DashBoard-Tabela Crud</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp" />
+    <link rel="stylesheet" href="../css/style.css">
 
-    if(!$conn){
-        die("connection faile" . mysqli_connect_error());
-    }
-    echo "Connecao bem sucedida";
+</head>
 
+<body>
+    <div class="container">
+        <aside>
+            <div class="top">
+                <div class="logo">
+                    <a href="/html/index.html">
+                        <img src="../images/logo.PNG">
+                    </a>
+                </div>
+                <div class="close" id="close-btn">
+                    <span class="material-symbols-sharp">close</span>
+                </div>
+            </div>
+            <div class="sidebar">
+                <a href="../html/index.html">
+                    <span class="material-symbols-sharp">grid_view</span>
+                    <h3>DashBoard</h3>
+                </a>
+                <a href="../html/forms.html">
+                    <span class="material-symbols-sharp">description</span>
+                    <h3>Formulario</h3>
+                </a>
+                <a href="#" class="active">
+                    <span class="material-symbols-sharp">table_chart</span>
+                    <h3>Tabela Crud</h3>
+                </a>
+                <a href="../html/mapa.html">
+                    <span class="material-symbols-sharp">map</span>
+                    <h3>Mapa</h3>
+                </a>
+                <a href="../html/graficos.html">
+                    <span class=" material-symbols-sharp ">monitoring</span>
+                    <h3>Graficos</h3>
+                </a>
+                <a href="# ">
+                    <span class="material-symbols-sharp ">widgets</span>
+                    <h3>Componentes</h3>
+                </a>
+            </div>
+        </aside>
+        <!-- fim da sidebar -->
+        <main>
+            <h1 class="titulo">Tabela Crud</h1>
+            <div class="containerphp">
+                <a href="../html/forms.html" class="btn-add">Adicionar Novo</a>
+                <table>
+                    <tr>
+                        <th>Username</th>
+                        <th>Password</th>
+                        <th>Ações</th>
+                    </tr>
+                    <?php   
+                        //include "script.php";
+                        $conn = new mysqli('localhost','root','root','dashboard', 3305);
+                        $sql = "SELECT * FROM `registration` ";
+                        $result = mysqli_query($conn, $sql);
+                        while($row = mysqli_fetch_assoc($result)){
+                            ?>
+                                <tr>
+                                    <td><?php echo $row['username']?></td>
+                                    <td><?php echo $row['password']?></td>
+                                    <td>
+                                        <a href="#"><span class="material-symbols-sharp">edit_square</span></a>
+                                        <a href="#"><span class="material-symbols-sharp">delete</span></a>
+                                    </td>
+                                </tr>
+                            <?php
+                        }   
+                    ?>
+                </table>
+            </div>
+        </main>
+        <!--Fim da main-->
+        <div class="right ">
+            <div class="top ">
+                <button id="menu-btn ">
+                    <span class="material-symbols-sharp ">menu</span>
+                </button >
+                <!--notificacoes-->
+                <div class="alertas " onclick="alertasToggle(); ">
+                        <span class="material-symbols-sharp ">notifications</span>
+                </div>
+                <div class="alertas-context ">
+                    <ul>
+                        <li><a href="# "><span class="material-symbols-sharp ">sell</span>Nova venda realizada!</a></li>
+                        <li><a href="# "><span class="material-symbols-sharp ">euro</span>Euro subiu 2% relativamente ao dolar! </a></li>
+                        <li><a href="# "><span class="material-symbols-sharp ">payments</span>Pagamento por realizar!</a></li>
+                    </ul>
+                </div>
+                <!--profile dropdown -->
+                <div class="profile " onclick="menuToggle(); ">
+                    <img class="profile-photo " src="../images/profile.png ">
+                </div>    
+                <div class="menu-profile ">
+                    <h3>Goncalo Alves<br><small class="text-muted ">Web Developer</small></h3>
+                    <ul>
+                        <li><a href="# "><span class="material-symbols-sharp ">person</span>Profile</a></li>
+                        <li><a href="# "><span class="material-symbols-sharp ">settings</span>Settings</a></li>
+                        <li><a href="# "><span class="material-symbols-sharp ">logout</span>Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+            <!--top end-->
+            <div class="recent-updates ">
+                <h2>Updates Recentes</h2>
+                <div class="updates ">
+                    <div class="update ">
+                        <div class="profile-photo ">
+                            <img src="../images/profile.png ">
+                        </div>
+                        <div class="message ">
+                            <p><b>Goncalo Alves</b> recebeu o seu pedido da bola de futebol</p>
+                            <small class="text-muted ">3 minutos atras</small>
+                        </div>
+                    </div>
+                    <div class="update ">
+                        <div class="profile-photo ">
+                            <img src="../images/profile3.png ">
+                        </div>
+                        <div class="message ">
+                            <p><b>Goncalo Alves</b> recebeu o seu pedido das joelheiras</p>
+                            <small class="text-muted ">2 minutos atras</small>
+                        </div>
+                    </div>
+                    <div class="update ">
+                        <div class="profile-photo ">
+                            <img src="../images/profile1.png ">
+                        </div>
+                        <div class="message ">
+                            <p><b>Goncalo Alves</b> recebeu o seu pedido da bola de voleibol</p>
+                            <small class="text-muted ">1 hora atras</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--updates recentes end-->
+            <div class="sales-analytics ">
+                <h2>Analise Saldos</h2>
+                <div class="item online ">
+                    <div class="icon ">
+                        <span class="material-symbols-sharp ">shopping_cart</span>
+                    </div>
+                    <div class="right ">
+                        <div class="info ">
+                            <h3>Pedidos Online</h3>
+                            <small class="text-muted ">Ultimas 24 horas</small>
+                        </div>
+                        <h5 class="sucess ">+39%</h5>
+                        <h3>3849</h3>
+                    </div>
+                </div>
+                <div class="item offline ">
+                    <div class="icon ">
+                        <span class="material-symbols-sharp ">local_mall</span>
+                    </div>
+                    <div class="right ">
+                        <div class="info ">
+                            <h3>Pedidos Offline</h3>
+                            <small class="text-muted ">Ultimas 24 horas</small>
+                        </div>
+                        <h5 class="danger ">-17%</h5>
+                        <h3>1120</h3>
+                    </div>
+                </div>
+                <div class="item customers ">
+                    <div class="icon ">
+                        <span class="material-symbols-sharp ">person</span>
+                    </div>
+                    <div class="right ">
+                        <div class="info ">
+                            <h3>Novos Clientes</h3>
+                            <small class="text-muted ">Ultimas 24 horas</small>
+                        </div>
+                        <h5 class="sucess ">+25%</h5>
+                        <h3>569</h3>
+                    </div>
+                </div>
+                <div class="item add-product ">
+                    <div>
+                        <span class="material-symbols-sharp ">add</span>
+                        <h3>Adicionar Produto</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="../js/index.js"></script>
+</body>
 
-?>
+</html>
--- dashboard/php/script.php
+++ dashboard/php/script.php
@@ -39,7 +39,8 @@
     }
 */
 
-
+//script portatil
+/*
 $username = $_POST['username'];
 $password = $_POST['password'];
 
@@ -59,5 +60,25 @@ else{
 
 header("location: ../html/forms.html");
 exit();
+*/
+//script fixo
+$username = $_POST['username'];
+$password = $_POST['password'];
 
+$conn = new mysqli('localhost','root','root','dashboard', 3305);
+if($conn->connect_error){
+    die('Conexao falhou: ' .$conn->connect_error);
+}
+else{
+    $stmt = $conn->prepare("insert into registration(username, password) values(?, ?)");
+    $stmt->bind_param("ss",$username,$password);
+    $stmt->execute();
+    echo "registo feito";
+    $stmt->close();
+    $conn->close();
+    
+}
+
+header('Location: ../html/forms.html');
+exit();
 ?>
