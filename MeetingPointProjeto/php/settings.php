d6cbfaef388077b82981eee47767b1c30cc0115c
Gonçalo Alves <goncalo.alves@ua.pt>
2023-04-16 01:08:28 +0100

    updates

--- MeetingPointProjeto/php/settings.php
+++ MeetingPointProjeto/php/settings.php
@@ -5,27 +5,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MONITORIZACAO</title>
+    <title>DashBoard-Gestao Utilizadores</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp" />
     <link rel="stylesheet" href="../css/cssdeteste.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
-    <?php 
-        include_once('db_connection.php');
-        include('protect.php');
-    ?>
+    <script src="bootstrap.bundle.min.js / bootstrap.bundle.js"></script>
+    
+    <?php include_once('db_connection.php');
+include('protect.php');?>
+
 </head>
 
 <body>
-    <?php 
-        $sel = "SELECT * FROM users ";
-        $query = mysqli_query($connect,$sel);
-        $result = mysqli_fetch_assoc($query);
-
-    ?>
-    
     <div class="container" id="container">
-        <aside class="sidebar" id="mySidebar">
+    <aside class="sidebar" id="mySidebar">
             <div class="top" id="main" >
                 <div class="menu">
                 <h2 style="color:white; display: none;" id="nomeProjeto">MEETING POINT</h2>
@@ -34,9 +26,13 @@
                         <span class="material-symbols-sharp" id="closeside" style="display: none; color: white; justify-content: center;">close</span>
                     </a>
                 </div>
+             <!--   <div class="close" id="close-btn">
+                    <span class="material-symbols-sharp">close</span>
+                </div>  -->
+                
             </div>
             <div class="sidebar">
-                <a href="index3.php" class="active">
+                <a href="index3.php" >
                 <span class="material-symbols-sharp">distance</span>
                     <h3 id="dashboard">MONITORIZAÇÃO</h3>
                 </a>
@@ -47,11 +43,10 @@
                 <a href="mapa.php"><span class="material-symbols-sharp">air</span>
                     <h3 id="tabelacrud">WIND</h3>
                 </a>
-                <a href="settings.php">
+                <a href="settings.php" class="active">
                     <span class="material-symbols-sharp">manage_accounts</span>
-                    <h3 id="profile">ADMINISTRAdOR</h3>
+                    <h3 id="profile">ADMINISTRADOR</h3>
                 </a>
-                <!--<p>Bem vindo, <?php echo $result['Name'];?></p>-->
                 <a href="logout.php" id="traco">
                     <span class="material-symbols-sharp">logout</span>
                     <h3 id="logout">LOGOUT</h3>
@@ -59,23 +54,42 @@
             </div>
         </aside>
         <!-- fim da sidebar -->
-        <main >
-            <h1 class="titulo">Monitorização</h1>
-            <!--
-            <script>
-                //alerta para reuniao
-                alert("falar cnu's");
-            </script>
--->
+        <main>
+            <h1 class="titulo">Gestao Utilizadores</h1>
+            
+            <form action="#" style="background: var(--color-background); border-radius: var(--border-radius-1); padding: var(--card-padding);">
+            <!--<h2>Gestão de Utilizadores</h2>-->
+            <br>
+            <br>
+            <label class="form-label" for="email">Email<i class="required">: </i></label>
+            <input id="email" type="email" placeholder="Email" name="email" required >
+            <br>
+            <br>
+            <label class="form-label" for="password">Password<i class="required">: </i></label>
+            <input id="password" type="password" class="form-control" placeholder="A sua password" name="password" required>
+            <br>
+            <br>
+            <label class="form-label" for="password">Confirmar Password<i class="required">: </i></label>
+            <input id="check_password" type="password" class="form-control" placeholder="Confirme a sua password" name="check_password" required>
+            <br>
+            <br>
+            <label for="users">Escolher Tipo Usuario: </label>
+            <select name="users" id="users">            
+                <option value="user">User</option>
+                <option value="admin">Admin</option>                   
+            </select>
+            <br>
+            <br>
+            <input type="submit" class="btn btn-primary" value="Registar">
+            </form>
         </main>
         <!--Fim da main-->
-        
+ 
     </div>
-
-
-
+    
     <script src="../js/index3.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+
 </body>
 
 </html>
